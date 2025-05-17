837a990c5172ffe15c4d6073c243ebc88c8b80e1
Ivan Fioravanti <ivan.fioravanti@gmail.com>
2025-05-17 08:07:44 +0100

    Add docstring to providers package

---
+++ providers/__init__.py
@@ -0,0 +1,5 @@
+"""Provider-specific implementations for wine variety prediction.
+
+This package exposes helper modules that interact with various AI model
+providers to classify wine varieties.
+"""
